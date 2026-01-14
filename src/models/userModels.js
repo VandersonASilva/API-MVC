332fa7fa1f5c15dd73ac524e3ec24db29c96a81d
vanderrson silva <vandersonsilva6184@digitalcollege.internal>
2026-01-14 18:52:58 -0300

    commit

---
+++ src/models/userModels.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import db from "../config/database.js";
+
+const User = db.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUIDV4,
+      // o type UUIDV4 gera um id único automático, sem a necessidade de um campo autoIncrement.
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: true,
+  }
+);
+
+export default User;
