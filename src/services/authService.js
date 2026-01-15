d014a5ebe61af2eae7a6a5a1545367b08ff04c4c
vanderrson silva <vandersonsilva6184@digitalcollege.internal>
2026-01-14 21:11:12 -0300

    commit

---
+++ src/services/authService.js
@@ -0,0 +1,52 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+
+export async function register(name, email, password) {
+  const existUser = await userRepository.findByEmail(email);
+
+  if (existUser) {
+    throw { status: 400, message: "Email já cadastrado." };
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+  const user = await userRepository.create({
+    name,
+    email,
+    password: hashPassword,
+  });
+
+  return {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+  };
+}
+
+export async function login(email, password) {
+  const existUser = await userRepository.findByEmail(email);
+  if (!existUser) {
+    throw { status: 401, message: "Usuário não encontrado." };
+  }
+  const validPassword = await bcrypt.compare(password, existUser.password);
+  if (!validPassword) {
+    throw { status: 401, message: "Senha inválida." };
+  }
+
+  const token = jwt.sign(
+    { id: existUser.id, name: existUser.name, email: existUser.email },
+    JWT_SECRET,
+    { expiresIn: JWT_EXPIRES_IN }
+  );
+
+  return {
+    token,
+    user: {
+      id: existUser.id,
+      name: existUser.name,
+      email: existUser.email,
+    },
+  };
+}
